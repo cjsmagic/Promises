3fe14584c9578953f1c39feba7e94b7290f52535
cjsmagic <clarencemerchant1995@gmail.com>
2021-10-21 16:42:53 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function myPromise(status) {
+  return new Promise((resolve, reject) => {
+    if (status) {
+      resolve('Passed');
+    } else {
+      reject('Failed');
+    }
+  });
+}
+
+function noop() {}
+
+// check resolved
+myPromise(true).then((status) => {
+  console.log(status);
+}, noop);
+
+// check rejected
+myPromise(false).then(noop, (status) => {
+  console.log(status);
+});
+
+// with async await
+async function asyncFunc(status) {
+  try {
+    const value = await myPromise(status);
+    console.log('async', value);
+  } catch (e) {
+    console.log('async', e);
+  }
+}
+
+asyncFunc(true);
+asyncFunc(false);
